cf1e775886653cf710e1d04a3973ffa3c82a8303
florence ajadi <florence.ajadi@yahoo.com>
2022-05-05 22:31:32 -0500

    commit tree 2ef0433b555954c1bf1b533acd4d09bd543d31a0

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  fetch("https://anapioficeandfire.com/api/books")
+  .then((resp) => resp.json())
+  .then((json) => console.log(json));
 }
+
+
 
 function renderBooks(books) {
   const main = document.querySelector('main');
@@ -14,4 +18,8 @@ function renderBooks(books) {
 
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
+
+  
 });
+
+
